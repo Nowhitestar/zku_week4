61e9c333410aecd084215ce51864ac18f6fea7e6
Nowhitestar <nowhitestar@gmail.com>
2022-05-29 21:15:32 +0800

    week4 submission

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,10 +1,15 @@
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
+import { providers , Contract, utils } from "ethers"
 import Head from "next/head"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styles from "../styles/Home.module.css"
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json";
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
@@ -59,6 +64,55 @@ export default function Home() {
         }
     }
 
+    // new greetings
+    const [greeting, setGreeting] = useState("");
+    useEffect(() => {
+        const listenForGreeting = async () => {
+          const provider = (await detectEthereumProvider()) as any;
+          const ethers = new providers.Web3Provider(provider);
+    
+          const contract = new Contract(
+            "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+            Greeter.abi,
+            ethers
+          );
+    
+          contract.on("NewGreeting", (greeting: string) => {
+            setGreeting(utils.parseBytes32String(greeting));
+          });
+        };
+    
+        listenForGreeting().catch(console.error);
+      }, []);
+
+    // form
+    interface IFormInputs {
+        firstName: string;
+        lastName: string;
+        age: number;
+        address: string;
+    }
+
+    const SignupSchema = yup
+        .object({
+            firstName: yup.string().required(),
+            lastName: yup.string().defined(),
+            age: yup.number().required().positive().integer(),
+            address: yup.string().required()
+        })
+        .required();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<IFormInputs>({
+        resolver: yupResolver(SignupSchema)
+    });
+
+    const onSubmit = (data: IFormInputs) => {
+        alert(JSON.stringify(data));
+    };
+
     return (
         <div className={styles.container}>
             <Head>
@@ -77,7 +131,40 @@ export default function Home() {
                 <div onClick={() => greet()} className={styles.button}>
                     Greet
                 </div>
+
+                <p className={styles.description}>New greetings below</p>
+                <div className={styles.logs}>{greeting}</div>
+                
+
+                <div className={styles.logs}></div>
+
+                <h1 className={styles.title}>Contact Us</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label>First Name</label>
+                        <input {...register("firstName")} />
+                        {errors.firstName && <p>{errors.firstName.message}</p>}
+                    </div>
+                    <div style={{ marginBottom: 10 }}>
+                        <label>Last Name</label>
+                        <input {...register("lastName")} />
+                        {errors.lastName && <p>{errors.lastName.message}</p>}
+                    </div>
+                    <div>
+                        <label>Age</label>
+                        <input type="number" {...register("age", { valueAsNumber: true })} />
+                        {errors.age && <p>{errors.age.message}</p>}
+                    </div>
+                    <div>
+                        <label>Address</label>
+                        <input {...register("address")} />
+                        {errors.address && <p>{errors.address.message}</p>}
+                    </div>
+                    <input type="submit" />
+                </form>
             </main>
+
         </div>
+
     )
 }
